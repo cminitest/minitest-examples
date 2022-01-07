fa7ab7e5e55c43d31294bcc24c0b58db440dc1df
Benjamin Anderson <andeb2804@gmail.com>
2022-01-07 16:19:33 -0600

    Add array example

--- assertions/equal/main.c
+++ assertions/equal/main.c
@@ -8,6 +8,20 @@ describe("equal assertion", test_equal_assertion)
   it("can check if not equal")
     expect(1) to not equal(0)
   end
+
+  context("arrays")
+    int x[] = {1,5,2};
+
+    it("can check if equal")
+      int y[] = {2,5,1};
+      expect(x) to equal(y)
+    end
+  
+    it("can check if not equal")
+      int y[] = {1,5,5};
+      expect(x) to not equal(y)
+    end
+  end
 end
 
 int main(void) {
